d52e49510072667381f76cb19af778d6ccf40f6e
dramos <dario.fenix@gmail.com>
2017-07-14 02:23:47 -0300

    Credentials error fix

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="dario-ramos", channel="testing")
+    builder.password = os.getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="conan_test:shared")
     builder.run()
